565e09feeaeb708f24866b447106e878095d777a
sandhyalal <sandhyalalkumar@gmail.com>
2015-11-03 02:28:41 +0530

    sorted arrays union and intersection

--- C++/array/find_union_and_intersection.cpp
+++ C++/array/find_union_and_intersection.cpp
@@ -35,18 +35,42 @@ void find_union(int set_one[], int set_two[], int onelen, int twolen){
     cout<<endl;
 }
 void find_intersect(int set_one[], int set_two[], int onelen, int twolen){
+	int i=0;
+	int j=0;
+	int k=0;
+	int len = onelen > twolen ? onelen : twolen;
+	int *intersect_array = new int[len];
+
+	while(i < onelen && j < twolen){
+		if( set_one[i] < set_two[j]){
+			i++;
+			continue;
+		}
+		else if(set_one[i] == set_two[j]){
+			intersect_array[k++] = set_one[i];
+			i++;
+			j++;
+		}
+		else{
+			j++;
+		}
+
+	}
+	for(int i=0;i<len;i++)
+    	cout<<intersect_array[i]<<" ";
+    cout<<endl;
 
 }
 
 int main(){
 
-	int set_one[] = {1,2,3,4,5,6,7,8,12};
-	int set_two[] = {2,4,5,6,9,10,11,12,13,14};
+	int set_one[] = {1,2,3,4,5,6,7,8,12,15};
+	int set_two[] = {2,4,5,6,9,10,11,12,13,14,15};
 
 	int onelen = sizeof(set_one)/sizeof(int);
 	int twolen = sizeof(set_two)/sizeof(int);
 
 	find_union(set_one, set_two, onelen, twolen);
-	//find_intersect(int set_one[], int set_two[], int onelen, int twolen);
+	find_intersect(set_one, set_two, onelen, twolen);
 	return 0;
 }
