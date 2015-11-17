0b3c460e45dd94632587c241d4865d826770247d
sandhyalal <sandhyalalkumar@gmail.com>
2015-11-17 09:29:17 +0530

    DAG using adj matrix

--- C++/dynamic_programming/dag_dynamic_programming.cpp
+++ C++/dynamic_programming/dag_dynamic_programming.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 int main(){
-	
+
+	int vertex = 6;
+	int adjmat[][6] = { { 0,0,0,0,0,0 },
+	                               { 0,0,0,0,0,0 },
+	                               { 0,0,0,0,0,0 },
+	                               { 0,0,0,0,0,0 },
+	                               { 0,0,0,0,0,0 },
+	                               { 0,0,0,0,0,0 } };
+
+	map <char, int> vertexmap;
+	vertexmap['A'] = 0;
+	vertexmap['B'] = 1;
+	vertexmap['C'] = 2;
+	vertexmap['D'] = 3;
+	vertexmap['E'] = 4;
+	vertexmap['F'] = 5;
+
+	//cout<<vertexmap['E']<<endl;
+
 	return 0;
 }
