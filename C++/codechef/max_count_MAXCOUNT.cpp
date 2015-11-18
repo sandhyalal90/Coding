a8469c4dc2776f9f6e5d50a280ac06a4927d2c71
sandhyalal <sandhyalalkumar@gmail.com>
2015-11-18 08:26:45 +0530

    Easy problem accepted

---
+++ C++/codechef/max_count_MAXCOUNT.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+using namespace std;
+
+int main(){
+
+  int T;
+  scanf("%d",&T);
+
+  while(T--){
+  int A[10001];
+  for(int i=0;i<10001;i++)
+  A[i] = 0;
+  int N;
+  scanf("%d",&N);
+  int K;
+  for(int i=0;i<N;i++){
+  scanf("%d",&K);
+  A[K]++;
+  }
+  int maxcount = 0;
+  int max = 0;
+  for(int i=0;i<10001;i++){
+    if(A[i] > maxcount){
+      maxcount = A[i];
+      max = i;
+    }
+  }
+  printf("%d %d\n",max,maxcount);
+  }
+return 0;
+}
