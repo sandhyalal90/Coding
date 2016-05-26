dce341068e63ea57e3f03e68e2ca5f569d6f0da8
sandhyalal3loq <sandhyalal@3loq.com>
2016-05-26 16:38:56 +0530

    More codes have been added

--- C++/codechef/covering_ZCO15003.cpp
+++ C++/codechef/covering_ZCO15003.cpp
@@ -1,18 +1,33 @@
+#include <bits/stdc++.h>
 #include <iostream>
-
 using namespace std;
+ 
+#define pair1 pair<int,int>
+ 
+int main() 
+{
+	int i,n,ans=1;
+	scanf("%d",&n);
+	pair1 p[n],cur;
+	for(i=0;i<n;i++)
+	 scanf("%d%d",&p[i].first, &p[i].second);
+     sort(p,p+n);
 
-int main(){
-	int N;
-	scanf("%d",&N);
- 
-    int *aa = new int[];
-    int *bb = new int[];
-
-    for(int i=0;i<N;i++){
-    	scanf("%d",&aa[i]);
-    	scanf("%d",&bb[i]);
-    }
-    
-    return 0;
-}
+	cur=p[0];
+	for(i=1;i<n;i++)
+	{
+		if(p[i].first<=cur.second)
+		{
+			cur.first=p[i].first;
+			if(p[i].second<cur.second)
+			 cur.second=p[i].second;
+		}
+		else
+		{
+			cur=p[i];
+			ans++;
+		}
+	}
+	printf("%d\n",ans);
+	return 0;
+} 
